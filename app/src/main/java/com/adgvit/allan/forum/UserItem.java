87c36b376d9883859d15baf51c100d54218fcef7
Allan Bastian <allan.batian9.ab@gmail.com>
2017-08-10 14:39:31 +0530

    Added user question layout

---
+++ app/src/main/java/com/adgvit/allan/forum/UserItem.java
@@ -0,0 +1,15 @@
+package com.adgvit.allan.forum;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Allan on 09-08-2017.
+ */
+
+public class UserItem {
+    private CardView cardView;
+    private TextView userQues;
+}
